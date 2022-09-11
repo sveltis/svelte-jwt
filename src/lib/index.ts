a13d3fbc4e2ca4fdac3593541fb7af0c672a9519
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2022-09-11 20:40:30 +0200

    update test

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,88 @@
 import type { Handle } from '@sveltejs/kit';
-import { SvelteJWTHelper, type JWTGenerate } from '../classes';
+import * as jose from 'jose';
+
+export type JWTGenerate = <T>(payload: T) => Promise<string>;
+
+export interface ISvelteJWTHelper<T> {
+	parse(jwt: string): Promise<T>;
+	generate: JWTGenerate;
+	attachBearer(jwt: string): void;
+}
+export type SvelteKey = jose.KeyLike;
+
+type SvelteHelperProps<T> = {
+	issuer: string;
+	audience: string;
+	expirationTime?: string;
+	key: SvelteKey;
+	payloadDefault: T;
+};
+
+export class SvelteJWTHelper<T> implements ISvelteJWTHelper<T> {
+	private _issuer: string;
+	private _audience: string;
+	private _key: SvelteKey;
+	private _payloadDefault: T;
+	private _bearer: string | undefined;
+	private _expirationTime: string;
+
+	public constructor({
+		issuer,
+		audience,
+		key,
+		payloadDefault,
+		expirationTime
+	}: SvelteHelperProps<T>) {
+		this._issuer = issuer;
+		this._audience = audience;
+		this._key = key;
+		this._payloadDefault = payloadDefault;
+		this._expirationTime = expirationTime || '2h';
+	}
+
+	public static createKey = async (): Promise<SvelteKey> => {
+		return (await jose.generateSecret('HS256')) as SvelteKey;
+	};
+
+	public attachBearer = (jwt: string): void => {
+		this._bearer = jwt;
+	};
+
+	public parse = async (): Promise<T> => {
+		try {
+			if (!this._bearer) return this._payloadDefault;
+			const { payload } = await jose.jwtDecrypt(this._bearer, this._key, {
+				issuer: this._issuer,
+				audience: this._audience
+			});
+			const result = this._payloadDefault;
+			for (const key in this._payloadDefault) {
+				if (Object.prototype.hasOwnProperty.call(this._payloadDefault, key) && payload?.[key]) {
+					result[key] = (payload as unknown as T)[key];
+				}
+			}
+
+			return result;
+		} catch (error) {
+			throw new Error('Parsing JWT error');
+		}
+	};
+
+	public generate = async <T>(payload: T): Promise<string> => {
+		try {
+			const jwt = await new jose.EncryptJWT({ ...payload } as jose.JWTPayload)
+				.setProtectedHeader({ alg: 'dir', enc: 'A256GCM' })
+				.setIssuedAt()
+				.setIssuer(this._issuer)
+				.setAudience(this._audience)
+				.setExpirationTime(this._expirationTime)
+				.encrypt(this._key);
+			return jwt;
+		} catch (error) {
+			throw new Error('JWT can not be created');
+		}
+	};
+}
 
 const key = await SvelteJWTHelper.createKey();
 
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,23 +1,86 @@
-import { assert, describe, expect, it } from 'vitest';
-import { SvelteJWTHelper } from '../src/classes/index';
-import * as jose from 'jose';
+import { describe, expect, it } from 'vitest';
+import { SvelteJWTHelper } from '../src/lib/';
 
 const key = await SvelteJWTHelper.createKey();
 
-type JWTPayload = {
+type SimplePayload = {
 	id: string;
 };
 
+type ComplexPayload = {
+	id: number;
+	str: string;
+	bool: boolean;
+};
+
+const [issuer, audience] = ['iss', 'aud'];
+
 describe('SvelteJWTHelper', () => {
 	it('shoud create a JWT token', async () => {
-		const [issuer, audience] = ['iss', 'aud'];
-		const instance = new SvelteJWTHelper<JWTPayload>({
+		const instance = new SvelteJWTHelper<SimplePayload>({
 			issuer,
 			audience,
 			key,
 			payloadDefault: { id: 'unknown' }
 		});
-		const token = instance.generate({ id: 123 });
-		expect(token).toBeTruthy();
+		const token = await instance.generate({ id: 123 });
+		expect(typeof token).toBe('string');
+	});
+
+	it('should return default payload if token is not defined', async () => {
+		const id = 'unknown';
+		const instance = new SvelteJWTHelper<SimplePayload>({
+			issuer,
+			audience,
+			key,
+			payloadDefault: { id }
+		});
+
+		const result = await instance.parse();
+		expect(result.id).toBe(id);
+	});
+
+	it('should parse object', async () => {
+		const payload = {
+			id: 123,
+			str: 'string',
+			bool: true
+		};
+
+		const instance = new SvelteJWTHelper<ComplexPayload>({
+			issuer,
+			audience,
+			key,
+			payloadDefault: { id: 0, str: '', bool: false }
+		});
+		const token = await instance.generate(payload);
+
+		instance.attachBearer(token);
+
+		const object = await instance.parse();
+		expect(object.id).toBe(payload.id);
+		expect(object.bool).toBe(payload.bool);
+		expect(object.str).toBe(payload.str);
+	});
+
+	it('should not contain other data', async () => {
+		const payload: ComplexPayload = {
+			id: 123,
+			str: 'string',
+			bool: true
+		};
+
+		const instance = new SvelteJWTHelper<ComplexPayload>({
+			issuer,
+			audience,
+			key,
+			payloadDefault: { id: 0, str: '', bool: false }
+		});
+		const token = await instance.generate(payload);
+		instance.attachBearer(token);
+		const result = await instance.parse();
+		Object.keys(result).forEach((el) => {
+			expect(Object.keys(payload)).includes(el);
+		});
 	});
 });
